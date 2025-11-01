80970e30a83ebd10194220f1731452abd358594f
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-01 18:45:27 +0100

    Update links_ids.py

--- config/links_ids.py
+++ config/links_ids.py
@@ -1,8 +1,8 @@
 def get_groups():
     new_users_ID = -1002515925429
     redeemed_keys_ID = -1002562333792
-    vouches_LINK, vouches_ID = 'https://t.me/CROWS_Vouches', -1002702998468
-    main_channel_link, main_channel_ID = 'https://t.me/CROWsNewsProofs', -1002824424675
+    vouches_LINK, vouches_ID = 'https://t.me/+YmPZwIxpanA5NTRk', -1002702998468
+    main_channel_link, main_channel_ID = 'https://t.me/DRAGON_OTP_channel', -1002269511043
 
     return {
         "new_users_ID": new_users_ID,
