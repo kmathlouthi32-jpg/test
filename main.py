d4556dc1b00226ea256517e1ef883906a1bd0656
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-18 19:27:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+from utils import keep_alive
+
+keep_alive()
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 import asyncio
-from utils import init_db, create_tables, escape_markdown, keep_alive
+from utils import init_db, create_tables, escape_markdown
 from handlers import *
 import psutil, os
 
-keep_alive()
-
 bot = Bot(token='7886245319:AAGP1f1WQ_1Baw5ewNNlHTa6JsWRud5GP1Q')
 dp = Dispatcher()
-
-
-
-
 
 
 def get_memory_usage():
@@ -20,10 +17,15 @@ def get_memory_usage():
     mem = process.memory_info().rss / (1024 * 1024)  # Convert bytes to MB
     return round(mem, 2)
 
+
 # Example: log it every few minutes
 async def log_memory():
     while True:
-        await bot.send_message(-1002942544591,text=f"💾 Memory usage: *{escape_markdown(str(get_memory_usage()))}* MB",parse_mode='MarkdownV2')
+        await bot.send_message(
+            -1002942544591,
+            text=
+            f"💾 Memory usage: *{escape_markdown(str(get_memory_usage()))}* MB",
+            parse_mode='MarkdownV2')
         await asyncio.sleep(300)
 
 
@@ -34,7 +36,8 @@ dp.message.register(start_command, Command(commands=["start"]))
 
 # CALLBACK
 dp.callback_query.register(help_callback, lambda c: c.data == "help")
-dp.callback_query.register(start_callback, lambda c: c.data in ['back1','back4'])
+dp.callback_query.register(start_callback,
+                           lambda c: c.data in ['back1', 'back4'])
 
 # SUBSCRIPTION
 # COMMANDS
@@ -43,14 +46,20 @@ dp.message.register(my_profile_command, Command(commands=["plan"]))
 dp.message.register(redeem_keys, Command(commands=["redeem"]))
 
 # CALLBACKS
-dp.callback_query.register(wallets_callback, lambda c: c.data in ['20','50','90','200'])
+dp.callback_query.register(wallets_callback,
+                           lambda c: c.data in ['20', '50', '90', '200'])
 dp.callback_query.register(wallet_callback, lambda c: ':' in c.data)
 dp.callback_query.register(purchase_callback, lambda c: c.data == "purchase")
 dp.callback_query.register(my_profile_callback, lambda c: c.data == "plan")
 
-
-# CALL 
-dp.message.register(call_command, Command(commands=['repcall','recall',"call","paypal", "venmo", "applepay", "coinbase", "microsoft", "amazon", "quadpay", "cashapp", "citizens", "marcus", "carrier",'creditcard']))
+# CALL
+dp.message.register(
+    call_command,
+    Command(commands=[
+        'repcall', 'recall', "call", "paypal", "venmo", "applepay", "coinbase",
+        "microsoft", "amazon", "quadpay", "cashapp", "citizens", "marcus",
+        "carrier", 'creditcard'
+    ]))
 dp.message.register(Phonelist_commands, Command(commands=["phonelist"]))
 
 # CALLBACKS
@@ -58,14 +67,17 @@ dp.callback_query.register(otp_accept_callback, lambda c: c.data == "acp")
 
 # STTINGS
 # COMMANDS
-dp.message.register(voicelist_command, Command(commands=["voicelist"]))  # NEED WORK
+dp.message.register(voicelist_command,
+                    Command(commands=["voicelist"]))  # NEED WORK
 dp.message.register(setvoice_command, Command(commands=["setvoice"]))
 dp.message.register(setscript_command, Command(commands=["setscript"]))
 dp.message.register(process_script_text, ScriptForm.waiting_for_script)
 dp.message.register(view_script, Command(commands=["script"]))
 
 # CALLBACK
-dp.callback_query.register(changevoice_callback, lambda c: c.data in ['Michael','Ethan','Mark','Mia','Sofia','Andria'])
+dp.callback_query.register(
+    changevoice_callback,
+    lambda c: c.data in ['Michael', 'Ethan', 'Mark', 'Mia', 'Sofia', 'Andria'])
 
 # ADMIN
 # COMMANDS
@@ -75,11 +87,17 @@ dp.message.register(keys_command, Command(commands=["keys"]))
 dp.message.register(generate_keys_command, Command(commands=["gkeys"]))
 
 # CALLBACKS
-dp.callback_query.register(keys_callback, lambda c: c.data=='keys')
-dp.callback_query.register(generate_keys_callback, lambda c: c.data=='g_keys')
-dp.callback_query.register(get_keys_callback, lambda c: c.data in ['2 hours','1 day','4 days','1 week','1 month'])
+dp.callback_query.register(keys_callback, lambda c: c.data == 'keys')
+dp.callback_query.register(generate_keys_callback,
+                           lambda c: c.data == 'g_keys')
+dp.callback_query.register(
+    get_keys_callback,
+    lambda c: c.data in ['2 hours', '1 day', '4 days', '1 week', '1 month'])
 
-dp.message.register(unknown_command,lambda message: message.text and message.text.startswith('/'))
+dp.message.register(
+    unknown_command,
+    lambda message: message.text and message.text.startswith('/'))
+
 
 async def main():
     print("Bot is running...")
@@ -88,8 +106,6 @@ async def main():
     asyncio.create_task(log_memory())
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
-
