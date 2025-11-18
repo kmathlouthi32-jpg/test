1676b29f2d584daffc0beda108c9fd9fb7fb4dee
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-18 21:14:02 +0100

    Update keepalive.py

--- utils/keepalive.py
+++ utils/keepalive.py
@@ -3,15 +3,12 @@ from threading import Thread
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
     return "I'm alive!"
 
-
 def run():
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=10000)
 
 def keep_alive():
     t = Thread(target=run)
