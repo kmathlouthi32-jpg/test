9b1d71b7e4ff0a191ad90affeb04dba75f8798f4
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-23 02:09:49 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,6 @@ from .text_utils import escape_markdown, is_name_valid
 from .payment_utils import get_wallet_message, check_subscription
 from .spoof_utils import check_spoof, get_spoofer_number,get_service_name ,get_service_name_bynum
 from .db import init_db, create_tables, add_user, set_user_value, get_user_info, user_exists, get_user_count, show_valid_keys, redeem_key, generate_bulk_keys
-from .keepalive import keep_alive
 
 
 
