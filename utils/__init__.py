46efd46b4b593aff5789e474b22dd050421298ef
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-28 23:31:08 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,8 @@ from .phone_utils import get_region_language, is_valid_phone_number
 from .text_utils import escape_markdown, is_name_valid
 from .payment_utils import get_wallet_message, check_subscription
 from .spoof_utils import check_spoof, get_spoofer_number,get_service_name ,get_service_name_bynum
-from .db import init_db, create_tables, add_user, set_user_value, get_user_info, user_exists, get_user_count, show_valid_keys, redeem_key, generate_bulk_keys
+from .database import db
+from .handlers_manager import get_user_cached, load_all_users, update_user_cache, is_new_user
 
 
 
