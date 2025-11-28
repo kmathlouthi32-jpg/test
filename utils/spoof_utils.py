34844d6d7c55a4496c0568976729ca080349b122
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-11-28 23:32:16 +0100

    Update spoof_utils.py

--- utils/spoof_utils.py
+++ utils/spoof_utils.py
@@ -1,4 +1,4 @@
-from config import get_spoofing, get_spoofing_services, get_servies
+from config import get_spoofing, get_spoofing_services, get_servies, get_pre_spoofing_services, get_pre_spoofing_numbers
 
 def check_spoof(spoof_number, service_name, name):
     if name.upper() in get_spoofing_services():
@@ -20,7 +20,7 @@ def check_spoof(spoof_number, service_name, name):
     return spoof_place == service_place
 
 def get_spoofer_number(service_name):
-    return get_spoofing()[get_spoofing_services().index(service_name.upper())]
+    return get_pre_spoofing_numbers()[get_pre_spoofing_services().index(service_name.upper())]
 
 def get_service_name(service_name):
         return service_name.lower()
