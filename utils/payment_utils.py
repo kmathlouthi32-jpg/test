1f15d55bd22d2309965545ad81fb827b6dd47973
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-12-14 12:46:31 +0100

    Update payment_utils.py

--- utils/payment_utils.py
+++ utils/payment_utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from .text_utils import escape_markdown
 import httpx
+from config import get_admin
 
 COINS = {
     "btc": "bitcoin",
@@ -61,21 +62,29 @@ def check_subscription(expiry_date):
     expire_date = datetime.strptime(str(expiry_date), "%Y-%m-%d %H:%M:%S.%f")
     return expire_date > now
 
-def get_wallet_message(symbol: str, amount: float):
+def get_wallet_message(symbol: str, amount: float, wallet_type: int):
     now = datetime.now().strftime("%Y-%m-%d %H:%M")
     symbol = symbol.upper()
 
     plans = {15:'V.I.P Spoofer',20: '1 Day Plan', 50: '4 Days Plan', 90: '1 Week Plan', 200: '1 Month Plan', 1000: 'LifeTime Plan'}
-    wallets = {
+    wallets = [{
         'USDT': 'TRRVAuPEGJ4EgE33u1pV6gNUXxM1R5v1aY',
         'BTC': 'bc1q98y83fh28y6ysklu9qmla7enuegldmgdcdawvk',
         'ETH': '0xc76acc06684b2e2a2d43b9ba3b5f2618cd7a6307',
         'SOL': '8Ra9HKVrKNakEeQfqDzrVn1sFoQoFmbR51UHMRweT9hY',
         'LTC': 'LRJ8n55djedy4jyKP3Kkqi6iEy3BYC1FLt'
+    },
+    {
+        'USDT': 'THqWBtVxYRpWhgmMNd2M5nMkjTTVmsVgxh',
+        'BTC': '1M6Q4pFzofeBvA9e2CQ9rhtLyLkLc34p3q',
+        'ETH': '0x1c89c55def70cb0fccaf058abfc5a1e493d0e297',
+        'SOL': '5bPVRzUqc4ThfNST9uaKMn8PoS3xip1JxRShb8PWwWFW',
+        'LTC': 'LMWe7aWQkBcQZT5fzhfPDvZMHdYg9rwuwp'
     }
+    ]
 
     plan = plans.get(amount, "Unknown Plan")
-    wallet = wallets.get(symbol, "N/A")
+    wallet = wallets[wallet_type].get(symbol, "N/A")
 
     return fr"""ℹ *Payment Details*
 ━━━━━━━━━━━━━━━
@@ -86,4 +95,4 @@ def get_wallet_message(symbol: str, amount: float):
 💳 *Wallet:* `{wallet}`
 
 🔐 *To complete your purchase:*
-_Send the amount via the *{symbol}* wallet and send a screenshot to Support_\."""
+_Send the amount via the *{symbol}* wallet and send a screenshot to *[Support]({get_admin()['link']})*_\."""
