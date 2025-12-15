1b0e185cc5e6a0a3bb1fd543ec29644d4e6f3077
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-12-15 13:56:01 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,8 @@ from .text_utils import escape_markdown, is_name_valid
 from .payment_utils import get_wallet_message, check_subscription
 from .spoof_utils import check_spoof, get_spoofer_number,get_service_name ,get_service_name_bynum
 from .database import db
-from .handlers_manager import get_user_cached, load_all_users, update_user_cache, is_new_user
+from .handlers_manager import get_user_cached, load_all_users, update_user_cache, is_new_user,  get_all_users
+
 
 
 
