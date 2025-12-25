d942f9066da0e87f0cc9033afcc90e93872ad1e1
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-12-25 22:44:47 +0100

    Update text_utils.py

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,8 +1,20 @@
 import re
 from config import get_spoofing_services
 
-def escape_markdown(text: str) -> str:
+def escape_markdown_link(text: str) -> str:
+    escape_chars = r"*~`>#+-=|.!\\,"
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
+
+def escape_markdown_text(text: str) -> str:
+    escape_chars = r"_*[]()~`>#+-=|.!\\,"
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
+
+def escape_markdown_user(text: str) -> str:
     escape_chars = r"_*[]()~`>#+-=|{}.!\\,"
+    return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
+
+def escape_markdown(text: str) -> str:
+    escape_chars = r"*[]()~`>#+-=|.!\\,"
     return re.sub(f"([{re.escape(escape_chars)}])", r"\\\1", text)
 
 def is_name_valid(name: str):
