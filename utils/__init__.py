fc35bc03e96ed6034c1757927d217d50c2095cce
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2025-12-25 22:39:22 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,9 @@
 from .phone_utils import get_region_language, is_valid_phone_number
-from .text_utils import escape_markdown, is_name_valid
+from .text_utils import escape_markdown, is_name_valid, escape_markdown_user, escape_markdown_text
 from .payment_utils import get_wallet_message, check_subscription
 from .spoof_utils import check_spoof, get_spoofer_number,get_service_name ,get_service_name_bynum
 from .database import db
-from .handlers_manager import get_user_cached, load_all_users, update_user_cache, is_new_user,  get_all_users
-
-
-
-
+from .handlers_manager import get_user_cached, load_all_users, update_user_cache, is_new_user,  get_all_users, add_user_fast
+from .messages_manager import get_message, render_message, load_messages, preload_language, is_lang_exist
+from .translate import translate_button_text, fast_translate
+from .keyboard_manager import load_keyboards, get_keyboard, preload_language_keyboard
