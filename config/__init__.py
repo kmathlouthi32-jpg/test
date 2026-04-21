b868a0f914760e52b44de1d07785e5c0fcd72999
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2026-04-22 00:04:39 +0100

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -2,4 +2,6 @@ from .config import get_admin, get_error
 from .links_ids import get_groups
 from .services_spoofing import get_servies, get_spoofing, get_spoofing_services, spoof_message, get_pre_spoofing_services, get_pre_spoofing_numbers
 from .stickers import get_video, get_voices, get_proof
+from .scripts import get_scripts
+from .script_audios import get_script_audios
 
