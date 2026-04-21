e0cc52ad9aef2cb7968752baf8c42b2f166ec0fc
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2026-04-22 00:05:16 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,13 +4,4 @@ def get_admin():
         "id": 7674917466,
     }
 
-def get_error():
-    return (
-        r"""⚠ *ERROR*: *Access Restricted*
-Your access has been temporarily restricted due to one or more security or regional limitations\.
-Possible causes:
-• Potential security threat detected from your network\.
-• Call functionality is blocked due to device or network policy restrictions\.
-• Spoofing is not available in your region due to local telecom regulations\.
-Please verify your network environment or contact *support* if you believe this is a mistake\."""
-    )
+
