fe955a9bf9fd880e5ec91bab3d906b69b4e72c5f
kmathlouthi32-jpg <kmathlouthi32@gmail.com>
2026-04-22 00:08:47 +0100

    Update stickers.py

--- config/stickers.py
+++ config/stickers.py
@@ -1,17 +1,2 @@
 def get_video():
-    return "BAACAgQAAxkDAAIlZGkGQpFsHI5-UoiaIG4dRN_vmhIJAALaHgACpOoxUJPhIPtaQbPxNgQ"
-
-def get_proof():
-    return "BAACAgQAAxkDAAI1WGlEWL_UlHdInyVKw2rktwKoZlNWAAJaHAACFTghUn17o4L5OAruNgQ","BAACAgQAAxkDAAI1WWlEWO-aJjWSYlbhLPJR7nMFz_5hAAJcHAACFTghUgicnPVEz3oXNgQ"
-
-def get_voices():
-    return (
-        "CQACAgQAAxkDAAIlZWkGQpHqVT2GOpCgE9LPZc5KxySRAALbHgACpOoxUDVW1_akXNpHNgQ",
-        "CQACAgQAAxkDAAIlZmkGQpNWTZLYoXnvBHNxR-A4uHOjAALcHgACpOoxUO4vWVgthdJiNgQ",
-        "CQACAgQAAxkDAAIlZ2kGQpSDwlYozcMTT23x94VrDuVEAALdHgACpOoxUCHnc5QhHgqwNgQ",
-        "CQACAgQAAxkDAAIlaGkGQpZoojJMNkjMk0wbWwhEwtmlAALeHgACpOoxUFwW6MvDSevSNgQ",
-        "CQACAgQAAxkDAAIlaWkGQpcyqyibcF1OqO4MFH3mfVM8AALfHgACpOoxUEbQbOz2d9f-NgQ",
-        "CQACAgQAAxkDAAIlamkGQpiLb6G31F5ckxxSrWhESy9WAALgHgACpOoxUBSxN5JSBMQcNgQ",
-    )
-
-
+    return 'BAACAgQAAxkDAAIlZGkGQpFsHI5-UoiaIG4dRN_vmhIJAALaHgACpOoxUJPhIPtaQbPxNgQ'
